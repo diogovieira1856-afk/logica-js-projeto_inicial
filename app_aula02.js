be4dd01830b1165c490be34e08f92dffca59bec1
Diogo Alves <diogo.vieira1856@gmail.com>
2026-05-28 13:32:40 -0300

    Conclusão Aula 01

---
+++ app_aula02.js
@@ -0,0 +1,10 @@
+alert('Estou aprendendo JavaScript');
+let nomeUsuario = prompt('Digite seu nome');
+let idadeUsuario = prompt('Digite sua idade');
+
+alert('Olá '+ nomeUsuario + ', Você tem '+ idadeUsuario + ' Anos?')
+
+let temPao = prompt('Você tem pãio ? Responda com SIM ou NÃO');
+
+if( temPao=='sim'){alert('Fazer sanduíche');}
+else{alert('Fazer miojo');}
---
+++ app_exercicios.js
@@ -0,0 +1,31 @@
+//Ex-01
+alert ('Boas vindas ao nosso site!');
+
+//Ex-02
+let nome = 'lua'
+alert (nome);
+
+// Ex-03 
+let idade = 25
+
+// Ex-04 
+let numeroDeVendas = 50
+
+//Ex-05
+let saldoDisponivel = 1000
+
+//Ex-06
+alert('Erro! Preencha todos os campos');
+
+//Ex-07 
+let mensagemDeErro = 'Erro! Preencha todos os campos';
+alert(mensagemDeErro);
+
+//Ex-08
+let nome = prompt('Nome do Usuário:');
+
+//Ex-09
+let idade = prompt('Idade do usuário:');
+
+//Ex-10 
+if(idade>=18){alert('Pode tirar a habilitação!')};
