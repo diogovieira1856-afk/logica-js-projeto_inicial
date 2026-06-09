3314613249d13b1735c24a26338c30ce70c26646
Diogo Alves <diogo.vieira1856@gmail.com>
2026-06-09 17:46:00 -0300

    Aula 03

---
+++ app.js
@@ -0,0 +1,10 @@
+
+let diaDaSemana = prompt('Qual é o dia da semana?');
+
+if (diaDaSemana == "Sabado"){
+    alert('Bom fim de semana!')
+}else if(diaDaSemana == "Domingo"){
+ alert('Bom fim de semana!')
+}else {
+    alert('Boa semana!')
+}
