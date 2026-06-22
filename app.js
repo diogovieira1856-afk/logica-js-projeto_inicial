4983a763f4dc4165b20efd94f5ebb0e4284673f7
Diogo Alves <diogo.vieira1856@gmail.com>
2026-06-22 17:21:42 -0300

    Aula 04

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 alert('Boas vindas ao jogo do número secreto')
-let numeroSecreto = 8;
+let numeroSecreto = parseInt(Math.random()*100+1);
 console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 10:');
+let chute 
 // se chute for igual ao número secreto
-if (chute == numeroSecreto) {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}` );
-} else {
-    alert('Você errou :(')
-}
+let tentativas = 1
+while(numeroSecreto != chute) {
+    chute = prompt('Escolha um número entre 1 e 100:');
 
+    if (chute == numeroSecreto) {
+    break; 
+    }   
+    else {
+    if (chute>numeroSecreto){
+    alert(`O número secreto é menor que ${chute} !!`);}
+    else{
+    alert(`O número secreto é maior que ${chute} !!`);
+    }
+      tentativas++  
+    }
+} 
+//  Operador ternário 
+let palavratentativa = tentativas > 1 ? 'tentativas':'tentativa';
+ alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavratentativa}.` );
+ //if(tentativas>1){alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentatiivas.` );}
+ //else{( alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentatiiva.`));}
---
+++ app_exercicios_aula03.js
@@ -0,0 +1,29 @@
+// Exercicio-01
+//let contador =1
+//while(contador<11){
+//    alert(contador)
+//    contador++
+//}
+
+// Exercicio-02
+//let contador =10
+//while(contador>=0){
+//    alert(contador)
+//    contador--
+//}
+
+// Exercicio-03 
+//let numero = prompt('Digite um número:')
+//while(numero>=0){
+//    console.log(numero)
+//    numero--
+//}
+
+// Exercicio-04
+//let contador = 0
+//let numero = prompt('Digite um número:');
+//
+//while (contador<=numero){
+//    console.log(contador)
+//    contador++
+//}
