a3e2318e3fd57cb9fb8c48b30e6f1536163a12d6
Diogo Alves <diogo.vieira1856@gmail.com>
2026-06-22 14:48:10 -0300

    aula 02

--- app.js
+++ app.js
@@ -1,31 +1,11 @@
+alert('Boas vindas ao jogo do número secreto')
+let numeroSecreto = 8;
+console.log(numeroSecreto)
+let chute = prompt('Escolha um número entre 1 e 10:');
+// se chute for igual ao número secreto
+if (chute == numeroSecreto) {
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}` );
+} else {
+    alert('Você errou :(')
+}
 
-//Exercicio 01
-//let diaDaSemana = prompt('Qual é o dia da semana?');
-
-//if (diaDaSemana == "Sabado"){
-//    alert('Bom fim de semana!')
-//}else if(diaDaSemana == "Domingo"){
-// alert('Bom fim de semana!')
-//}else {
-//    alert('Boa semana!')
-//}
-
-//Exercicio 02
-//let numero = prompt('Digite um número:');
-
-//if (numero < 0){
-//    alert('Negativo')
-//}else if(numero >= 0){
-//    alert('Positivo')
-//}
-
-//Exercicio 03
-//let pontos = prompt ('Digite os pontos:');
-
-//if (pontos >= 100){
-//    alert ('Parabéns, você venceu!')
-//}else alert ('Tente novamente para ganhar')
-
-//Exercicio 04
-
- 
---
+++ app_exercicios_aula02.js
@@ -0,0 +1,42 @@
+
+//Exercicio 01
+//let diaDaSemana = prompt('Qual é o dia da semana?');
+
+//if (diaDaSemana == "Sabado"){
+//    alert('Bom fim de semana!')
+//}else if(diaDaSemana == "Domingo"){
+// alert('Bom fim de semana!')
+//}else {
+//    alert('Boa semana!')
+//}
+
+//Exercicio 02
+//let numero = prompt('Digite um número:');
+
+//if (numero < 0){
+//    alert('Negativo')
+//}else if(numero >= 0){
+//    alert('Positivo')
+//}
+
+//Exercicio 03
+//let pontos = prompt ('Digite os pontos:');
+
+//if (pontos >= 100){
+//    alert ('Parabéns, você venceu!')
+//}else alert ('Tente novamente para ganhar')
+
+//Exercicio 04
+
+//let saldo = 1250.50;
+//const mensagem = (`Olá! O saldo atual da sua conta é de R$ ${saldo.toFixed(2).replace('.', ',')}.`);
+//alert (mensagem)
+
+//Exercicio 05
+
+//let nome = prompt("Qual é o seu nome ?");
+
+//const bemvindo =`Olá ${nome}, seja bem-vindo!`; 
+
+//alert (bemvindo);
+
