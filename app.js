f53327489a2f954b728eff7fc38dd42ebce18318
Diogo Alves <diogo.vieira1856@gmail.com>
2026-06-22 13:52:28 -0300

    outra aula

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 
-let diaDaSemana = prompt('Qual é o dia da semana?');
+//Exercicio 01
+//let diaDaSemana = prompt('Qual é o dia da semana?');
 
-if (diaDaSemana == "Sabado"){
-    alert('Bom fim de semana!')
-}else if(diaDaSemana == "Domingo"){
- alert('Bom fim de semana!')
-}else {
-    alert('Boa semana!')
-}
+//if (diaDaSemana == "Sabado"){
+//    alert('Bom fim de semana!')
+//}else if(diaDaSemana == "Domingo"){
+// alert('Bom fim de semana!')
+//}else {
+//    alert('Boa semana!')
+//}
+
+//Exercicio 02
+//let numero = prompt('Digite um número:');
+
+//if (numero < 0){
+//    alert('Negativo')
+//}else if(numero >= 0){
+//    alert('Positivo')
+//}
+
+//Exercicio 03
+//let pontos = prompt ('Digite os pontos:');
+
+//if (pontos >= 100){
+//    alert ('Parabéns, você venceu!')
+//}else alert ('Tente novamente para ganhar')
+
+//Exercicio 04
+
+ 
