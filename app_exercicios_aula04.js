878a1da9675c8c1d0600cf8ecb69c9f04e71c6ae
Diogo Alves <diogo.vieira1856@gmail.com>
2026-06-22 18:18:07 -0300

    Aula 04

---
+++ app_exercicios_aula04.js
@@ -0,0 +1,72 @@
+// Exercicio_01
+//let mensagem = ('Seja bem vindo!');
+//console.log(mensagem);
+
+//Exercicio_02
+//let nome = prompt('Qual é o seu nome?')
+//console.log(`Olá, ${nome}!`)
+
+//Exercicio_03
+//let nome = prompt('Qual é o seu nome?')
+//alert(`Olá, ${nome}!`)
+
+//Exercicio_04
+//let linguagem = prompt('Qual a linguagem de programação que você mais gosta?')
+//console.log(linguagem)
+
+//Exercicio_05
+//let valor1 = prompt('Digite um número:');
+//let valor2 = prompt('Digite outro número:');
+//let resultado = Number(valor1)+Number(valor2) ;
+
+//console.log(`A soma de ${valor1} e ${valor2} é igual a ${resultado}`)
+
+//Exercicio_06
+//let valor1 = prompt('Digite um número:');
+//let valor2 = prompt('Digite outro número:');
+//let diferenca = Math.abs(Number(valor1)-Number(valor2)) ;
+//console.log(`A diferença de ${valor1} e ${valor2} é igual a ${diferenca}`)
+
+//Exercicio_07
+//let idade = prompt('Qual é sua idade ?');
+//if(idade>=18 ){
+//    alert('Você é maior de idade');
+//}else{alert('Você é menor de idade')};
+
+//Exercicio_08 
+//let numero = prompt('Digite um número:')
+//
+//if(numero>0){
+//    alert('O número é positivo!')
+//}else if (numero == 0){
+//    alert('O número é zero!')
+//}else{
+//    alert('O número é Negativo!')
+//}
+
+//Exercicio_09
+//let numero = 1  ;
+//while(numero<=10){
+//    console.log(numero)
+//    numero++
+//}
+
+//Exercicio_10
+//let nota = prompt('Digite sua nota:')
+//if(nota>=7){
+//    console.log(`Sua nota foi ${nota}, você foi Aprovado!`)
+//}else{
+//    console.log(`Sua nota foi ${nota}, você foi Reprovado!`)
+//}
+
+//Exercicio_11
+//let numero = parseInt(Math.random()*100000 +1);
+//console.log(numero)
+
+//Execicio_12
+//let numero = parseInt(Math.random()*10 +1);
+//console.log(numero)
+
+//Exercicio_13
+//let numero = parseInt(Math.random()*1000 +1);
+//console.log(numero)
