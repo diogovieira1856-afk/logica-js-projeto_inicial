36d7f07ebeff4776c2eb10de7af2c7b4645292bc
Diogo Alves <diogo.vieira1856@gmail.com>
2026-07-01 17:59:57 -0300

    Desafio 02

---
+++ 02-js-curso-2-explore funções e listas/Desafio-02/app.js
@@ -0,0 +1,36 @@
+//Desafio 1
+//function exibirTextoNoConsole(texto){
+//console.log(texto)
+//}
+
+//exibirTextoNoConsole('Olá, Mundo!')
+
+//Desafio 02
+
+//function exibirTextoNoConsole(texto){
+//    console.log('Olá, ',texto,'!');  
+//}
+//
+//exibirTextoNoConsole('Diogo')
+
+//Desafio 03
+//function dobraDeNumero(numero){
+//    return 2*numero    
+//}
+//let numero1 = prompt('Digite um número:');
+//let dobro = dobraDeNumero(numero1);
+//
+//alert (`O dobro de ${numero1} é ${dobro}`)
+
+//Desafio 04
+
+//function mediaDeTresNumeros(n1,n2,n3){
+//    return (n1+n2+n3)/3
+//}
+//let nu1 = parseFloat(prompt('Digite o 1° número:'));
+//let nu2 = parseFloat(prompt('Digite o 2° número:'));
+//let nu3 = parseFloat(prompt('Digite o 3° número:'));
+//let media = mediaDeTresNumeros(nu1,nu2,nu3);
+//alert (`A media desses números são ${media}!!`)
+
+//Desafio 05
--- 02-js-curso-2-explore funções e listas/app.js
+++ 02-js-curso-2-explore funções e listas/app.js
@@ -6,12 +6,12 @@
 let  numeroSecreto = gerarNumeroAleatorio()
  // Funçao com parametros 
 function exibirTextoNaTela(tag,texto){
-let campo = document.querySelector(tag)
-campo.innerHTML = texto 
-
+let campo = document.querySelector(tag);
+campo.innerHTML = texto ;
+}
 exibirTextoNaTela('h1','Jogo do número secreto')
 exibirTextoNaTela('p','Escolha um número entre 1 e 10 ')
-}
+
 //Função sem retorno e sem parametro 
 function verificarChute(){
     let chute = document.querySelector('input').value;
